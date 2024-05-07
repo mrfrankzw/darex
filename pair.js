68fcdffe681bdca7569e5746b03703328d7be6b5
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-07 03:02:31 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Ubuntu)", "", ""]
+                browser: ["Safari (Ubuntu)", "", ""]
              });
              if(!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
                 await delay(1500);
