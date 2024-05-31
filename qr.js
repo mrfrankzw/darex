3182a36ba67d7dabba38e48e553c6167702325dc
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-31 03:23:28 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,15 +1,66 @@
-const express = require("express");
-const app = express();
-
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
 const pino = require("pino");
-let { toBuffer } = require("qrcode");
-const path = require("path");
-const fs = require("fs-extra");
-const { Boom } = require("@hapi/boom");
-const PORT = process.env.PORT || 5000;
-const MESSAGE =
-  process.env.MESSAGE ||
-  `
+const {
+	default: France_King,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function FLASH_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_France_King = France_King({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: ["FLASH-MD", "FLASH-MD", ""],
+			});
+
+			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
+			Qr_Code_By_France_Kingr.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: 'FLASH-MD-WA-BOT;;;=>' + b64data });
+	
+				   let FLASH_MD_TEXT = `
+*🚨 CONGRATULATIONS 🎊*
+
 *✅sᴇssɪᴏɴ ᴄᴏɴɴᴇᴄᴛᴇᴅ✅*
 *Made With 💜*
 *By ɢɪғᴛᴇᴅ ᴛᴇᴄʜ💜*
@@ -31,105 +82,29 @@ ______________________________
 ______________________________
 
 Use your Session ID Above to complete Bot Deployment.
-Don't Forget To Give Star⭐ To My Repo.
-`;
+Don't Forget To Give Star⭐ To My Repo`
+	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, {text:FLASH_MD_TEXT},{quoted:session})
 
-if (fs.existsSync("./gifted_baileys")) {
-  fs.emptyDirSync(__dirname + "/gifted_baileys");
-}
 
-app.use("/", async (req, res) => {
-  const {
-    default: GiftedWASocket,
-    useMultiFileAuthState,
-    Browsers,
-    delay,
-    DisconnectReason,
-    makeInMemoryStore,
-  } = require("@whiskeysockets/baileys");
-  const store = makeInMemoryStore({
-    logger: pino().child({ level: "silent", stream: "store" }),
-  });
-  async function GIFTED() {
-    const { state, saveCreds } = await useMultiFileAuthState(
-      __dirname + "/gifted_baileys",
-    );
-    try {
-      let Smd = GiftedWASocket({
-        printQRInTerminal: false,
-        logger: pino({ level: "silent" }),
-        browser: ["Gifted", "GiftedMd", ""],
-        auth: state,
-      });
 
-      Smd.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect, qr } = s;
-        if (qr) {
-          res.end(await toBuffer(qr));
-        }
-
-        if (connection == "open") {
-          await delay(3000);
-          let user = Smd.user.id;
-          let CREDS = fs.readFileSync(
-            __dirname + "/gifted_baileys/creds.json",
-          );
-          var Scan_Id = Buffer.from(CREDS).toString("base64");
-          // res.json({status:true,Scan_Id })
-          console.log(`
-====================  SESSION ID  ==========================                   
-SESSION-ID ==> ${Scan_Id}
--------------------   SESSION CLOSED   -----------------------
-`);
-
-          let msgsss = await Smd.sendMessage(user, {
-            text: `Gifted;;;${Scan_Id}`,
-          });
-          await Smd.sendMessage(user, { text: MESSAGE }, { quoted: msgsss });
-          await delay(1000);
-          try {
-            await fs.emptyDirSync(__dirname + "/gifted_baileys");
-          } catch (e) {}
-        }
-
-        Smd.ev.on("creds.update", saveCreds);
-
-        if (connection === "close") {
-          let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
-          // console.log("Reason : ",DisconnectReason[reason])
-          if (reason === DisconnectReason.connectionClosed) {
-            console.log("Connection closed!");
-            // GIFTED().catch(err => console.log(err));
-          } else if (reason === DisconnectReason.connectionLost) {
-            console.log("Connection Lost from Server!");
-            //  GIFTED().catch(err => console.log(err));
-          } else if (reason === DisconnectReason.restartRequired) {
-            console.log("Restart Required, Restarting...");
-            GIFTED().catch((err) => console.log(err));
-          } else if (reason === DisconnectReason.timedOut) {
-            console.log("Connection TimedOut!");
-            // GIFTED().catch(err => console.log(err));
-          } else {
-            console.log("Connection closed with bot. Please run again.");
-            console.log(reason);
-            //process.exit(0)
-          }
-        }
-      });
-    } catch (err) {
-      console.log(err);
-      await fs.emptyDirSync(__dirname + "/gifted_baileys");
-    }
-  }
-
-  GIFTED().catch(async (err) => {
-    console.log(err);
-    await fs.emptyDirSync(__dirname + "/gifted_baileys");
-
-    //// ©2024 MADE BY GIFTED TECH
-  });
+					await delay(100);
+					await Qr_Code_By_France_King.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					FLASH_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await FLASH_MD_QR_CODE()
 });
-
-app.listen(PORT, () =>
-  console.log(`App listened on port http://localhost:${PORT}`),
-);
+module.exports = router
