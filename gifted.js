888d937758add34bbc0403a4fc6bc9c908aa5d6b
Gifted Maurice <166598671+mouricedevs@users.noreply.github.com>
2024-11-19 18:47:35 +0300

    Gifted

--- gifted.js
+++ gifted.js
@@ -6,12 +6,9 @@ const PORT = process.env.PORT || 8000;
 let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
 app.use('/',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
-})
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
